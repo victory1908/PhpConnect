959b13e54c0ddbed36f56ad5b7fda2b0cc2c001b
victory1908 <victory1908@gmail.com>
2015-10-31 23:47:40 +0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
- define('HOST','mysql.hostinger.in');
- define('USER','u502452270_andro');
- define('PASS','belal_123');
- define('DB','u502452270_andro');
+ define('HOST','localhost');
+ define('USER','svqrzyrb_vic');
+ define('PASS','19081987');
+ define('DB','svqrzyrb_nlbtest');
  
  $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
