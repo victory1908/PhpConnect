8e3a6fcb248c331123d6b7de2003a46cd66dfa98
victory1908 <victory1908@gmail.com>
2015-11-01 00:36:58 +0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,16 @@
-<?php
- define('HOST','localhost');
- define('USER','svqrzyrb_vic');
- define('PASS','19081987');
- define('DB','svqrzyrb_nlbtest');
- 
- $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+<?php 
+  define('HOST','localhost'); 
+  define('USER','svqrzyrb_vic'); 
+  define('PASS','VTf5jo3V'); 
+  define('DB','svqrzyrb_nlbtest'); 
+   
+  $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+
+mysql_select_db("DB") or die("Could not open the db");
+         $showtablequery="SHOW TABLES FROM DB";
+         $query_result=mysql_query($showtablequery);
+         while($showtablerow = mysql_fetch_array($query_result))
+         {
+         echo $showtablerow[0]." ";
+         } 
+?>
